a659a3223d486bd111765ec4d0b4a3f82d02336a
Ashish Sasane <jayashri.sasane09@gmail.com>
2022-10-14 16:19:36 +0530

    Added actions method in util

--- src/main/java/utilClass/Util.java
+++ src/main/java/utilClass/Util.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -63,6 +64,14 @@ public class Util {
     	FileHandler.copy(Source, des);
     	
     	return path;
+    	
     }
+    	
+    public static void hoverToElement(WebDriver driver,WebElement element) {
+    	
+    	Actions act = new Actions(driver);
+    	act.moveToElement(element).perform();
+    	}
+    
 	
 }
